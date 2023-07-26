25ba238de40bd9e4c1192c4bfa3d15c52ca99c08
DevanshBhatia <137121210+DevanshBhatia@users.noreply.github.com>
2023-07-26 13:24:40 +0530

    Delete backend directory

--- backend/secrets.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    JWTSECRET:"abhckjglsjlg",
-    DB_LINK: "mongodb+srv://Joksepha:t87v5oMdd82SxiBH@cluster0.b0d2mtt.mongodb.net/?retryWrites=true&w=majority",
-    APP_EMAIL :"jampotom421@gmail.com",
-    APP_PASSWORD: "yxoxgusjckaadrkc",
-}
